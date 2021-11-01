b4b347c8d0cd9ab03a599e6f77358b82e39cdbb3
shadeshsaha <shadesh15-8599@diu.edu.bd>
2021-11-01 20:49:25 +0600

    Server name has changed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function run(){
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Running Ageless Holidays Server')
 })
 
 app.listen(port, () => {
